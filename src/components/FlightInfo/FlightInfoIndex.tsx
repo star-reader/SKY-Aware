cd9f228c33823ae441be852956b144a16fa939eb
star-reader <jinch2287@outlook.com>
2025-08-01 22:50:40 +0800

    添加antd-mobile库及其相关依赖，以增强项目的移动端组件支持。

---
+++ src/components/FlightInfo/FlightInfoIndex.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState, memo } from "react"
+import pubsub from 'pubsub-js'
+import getPlatform, { PlatformType } from "../../utils/getPlatform"
+import useWindowWidth from "../../hooks/common/useWindowWidth"
+import constants from "../../configs/constants"
+
+// 把数据存到这里，各个组件去引
+// 根据尺寸和平台选择不同的内容，如fluent card， 其他card；fluent card不需要考虑响应式；其他card考虑响应式
+export default memo(() => {
+    const [isShow, setIsShow] = useState(false)
+    const [data, setData] = useState<OnlineFlight | null>(null)
+    const [platform, setPlatform] = useState<PlatformType>('web')
+    const windowWidth = useWindowWidth()
+
+    useEffect(() => {
+        getPlatform().then(setPlatform);
+    }, []);
+    
+    useEffect(() => {
+        pubsub.subscribe('query-online-pilot',(_, data: OnlineFlight) => {
+            setIsShow(true)
+            setData(data)
+        })
+    }, [])
+
+    // !todo 设计card
+
+    return (
+        isShow && 
+            platform === 'windows' ? 
+                // windows fluent ui card
+                <></>
+             : 
+                // other card, props in window size
+                <></>
+            
+    )
+})
